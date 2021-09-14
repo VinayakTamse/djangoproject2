ede3c6360f1dc6e87d54b55a310f2413e6a245be
Vinayak Tamse <vinayaktamse01@gmail.com>
2021-09-14 14:22:44 +0530

    modified

--- mysite/restapiemployee/views.py
+++ mysite/restapiemployee/views.py
@@ -5,6 +5,7 @@ from .models import Employee, Student
 from .serializers import EmpSerializer, StuSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -24,16 +25,18 @@ class StudentAPI(viewsets.ViewSet):
         return Response(serilaize_data.data)
 
     def retrieve(self, request, pk):
-        stu_data = Student.objects.get(pk=pk)
-        serialize_data = StuSerializer(stu_data)
-        return Response(serialize_data.data)
+        if Student.objects.filter(pk=pk).exists():
+            stu_data = Student.objects.get(pk=pk)
+            serialize_data = StuSerializer(stu_data)
+            return Response(serialize_data.data)
+        return Response({'msg':'Not Found'}, status=status.HTTP_404_NOT_FOUND)
 
     def update(self, request, pk):
         stu_data = Student.objects.get(pk=pk)
         serilaize_data = StuSerializer(stu_data, data=request.data)
         if serilaize_data.is_valid():
             serilaize_data.save()
-            return Response({'msg':'Updated'})
+            return Response({'msg':'Updated'}, status=status.HTTP_202_ACCEPTED)
         return Response(serilaize_data.errors)
 
     def partial_update(self, request, pk):
@@ -41,19 +44,19 @@ class StudentAPI(viewsets.ViewSet):
         serilaize_data = StuSerializer(stu_data, data=request.data, partial=True)
         if serilaize_data.is_valid():
             serilaize_data.save()
-            return Response({'msg':'Updated'})
+            return Response({'msg':'Updated'}, status=status.HTTP_202_ACCEPTED)
         return Response(serilaize_data.errors)
 
     def create(self, request):
         stu_data = StuSerializer(data=request.data)
         if stu_data.is_valid():
             stu_data.save()
-            return Response({'msg':'Created'})
+            return Response({'msg':'Created'}, status=status.HTTP_201_CREATED)
         return Response(stu_data.errors)
 
     def destroy(self, request, pk):
         stu_instance = Student.objects.get(pk=pk)
         stu_instance.delete()
-        return Response({'msg':'deleted'})
+        return Response({'msg':'deleted'}, status=status.HTTP_204_NO_CONTENT)
 
     
